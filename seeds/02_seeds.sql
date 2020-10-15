89d6e137735847ad4b61cc2f02b526e55ebd7d85
Sherwin <sherwin.kwan92@gmail.com>
2020-10-15 16:52:14 -0700

    change folder structure

--- seeds/02_seeds.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO properties (owner_id, title, description, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, street, city, province, country, post_code, active)
-  VALUES 
-  (39, 'Potter Manor', 'Lily''s home', 30, 2, 3, 2, '1 Fake Street', 'Godric''s Hollow', 'Scotland', 'UK', '12A12B', true),
-  (19, 'Black Manor', 'censored', 10000, 3, 3, 3, '12 Grimmauld Place', 'London', 'England', 'UK', '12C12D', true),
-  (24, 'The Three Broomsticks', 'a pub', 20, 3, 4, 3, '1 Hogsmeade Alley', 'Hogsmeade', 'Scotland', 'UK', '12E12F', true),
-  (35, 'Tower of the Sun', 'not your home', 500, 5, 20, 5, '1 Royal Palace', 'Minas Tirith', 'Gondor', 'Middle-Earth', '12G12H', true),
-  (8, 'Lighthouse Labs Office', 'students stay overnight here', 0, 0, 2, 2, '600-401 West Georgia Street', 'Vancouver', 'BC', 'Canada', 'V6B5A1', true);
--- seeds/04_seeds.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO reviews (reservation_id, message, rating, property_id, guest_id)
-  VALUES 
-  (1, 'message', 4, 2, 36),
-  (2, 'message2', 5, 4, 25),
-  (3, 'message3', 3, 3, 17),
-  (4, 'message4', 2, 4, 19),
-  (5, 'message5', 1, 4, 17);
